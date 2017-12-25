c4611b343a03625dd28c7a9c6f4e3595e4c7b070
parkerziegler <parkerellisziegler@gmail.com>
2017-12-25 13:58:32 -0800

    Adding a recursive factorial function into the sum.py file.

--- sumFactorial.py
+++ sumFactorial.py
@@ -13,3 +13,17 @@ def sum(arr):
 print sum([1])
 print sum([1, 5, 132])
 print sum([76, 40, 97, 2])
+
+"""
+function to return the factorial of a given number.
+implements the same recursive technique as sum
+"""
+def factorial(num):
+    if num == 0:
+        return 1
+    
+    return num * factorial(num - 1)
+
+print factorial(3)
+print factorial(5)
+print factorial(10)
